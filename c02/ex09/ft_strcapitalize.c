af8971074c5fddb144435c6152618dfc85bfa841
Spuko <83892026+SpukoBR@users.noreply.github.com>
2024-09-14 17:53:02 -0300

    c02 to ex10 done

---
+++ c02/ex09/ft_strcapitalize.c
@@ -0,0 +1,22 @@
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i == 0 || (str[i - 1] < '0' || str[i - 1] > '9')
+			&& (str[i - 1] < 'A' || str[i - 1] > 'Z')
+			&& (str[i - 1] < 'a' || str[i - 1] > 'z'))
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] = str[i] - 32;
+		}
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] = str[i] + 32;
+		}
+		i++;
+	}
+	return (str);
+}
---
+++ c02/ex10/ft_strlcpy.c
@@ -0,0 +1,20 @@
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i] != '\0' && i + 1 < size)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (size > 0)
+	{
+		dest[i] = '\0';
+	}
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
