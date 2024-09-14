77b77fbb940d20c757f81067605508397349d827
Spuko <83892026+SpukoBR@users.noreply.github.com>
2024-09-14 17:45:13 -0300

    c01 done

---
+++ c01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,14 @@
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	temp;
+
+	i = 0;
+	while (i < size / 2)
+	{
+		temp = tab[i];
+		tab[i] = tab[size - 1 - i];
+		tab[size - 1 - i] = temp;
+		i++;
+	}
+}
---
+++ c01/ex08/ft_sort_in_tab.c
@@ -0,0 +1,24 @@
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	temp;
+	int	swapped;
+
+	swapped = 1;
+	while (swapped)
+	{
+		swapped = 0;
+		i = 0;
+		while (i < size - 1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				temp = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = temp;
+				swapped = 1;
+			}
+			i++;
+		}
+	}
+}
