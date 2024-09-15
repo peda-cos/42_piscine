44297078459cc60c67460b75ce209a07b62b7ab1
Spuko <83892026+SpukoBR@users.noreply.github.com>
2024-09-15 11:06:59 -0300

    feat: ft_putchar, main.c and Makefile

--- c00/ex01/ft_print_alphabet.c
+++ c00/ex01/ft_print_alphabet.c
@@ -1,13 +1,18 @@
 #include <unistd.h>
 
-void	ft_print_alphabet(void)
+void    ft_putchar(char c)
 {
-	char	c;
+	write(1, &c, 1);
+}
+
+void    ft_print_alphabet(void)
+{
+	char    c;
 
 	c = 'a';
 	while (c <= 'z')
 	{
-		write(1, &c, 1);
+		ft_putchar(c);
 		c++;
 	}
 }
