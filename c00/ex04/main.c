c0b879712cfa976c39489f946f7e8d782c8ddd3b
Spuko <83892026+SpukoBR@users.noreply.github.com>
2024-09-15 11:22:55 -0300

    feat: main.c, Makefile and ft_putchar

---
+++ c00/ex04/main.c
@@ -0,0 +1,9 @@
+void	ft_is_negative(int n);
+
+int	main(void)
+{
+	ft_is_negative(-1); // Deve imprimir 'N'
+	ft_is_negative(0);  // Deve imprimir 'P'
+	ft_is_negative(1);  // Deve imprimir 'P'
+	return (0);
+}
