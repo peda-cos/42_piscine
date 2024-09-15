d2aaed830bc1ab9e07dee3da73c0d34c46f4fdd5
Spuko <83892026+SpukoBR@users.noreply.github.com>
2024-09-15 13:39:32 -0300

    fix: norminette

--- c08/ex01/ft_boolean.h
+++ c08/ex01/ft_boolean.h
@@ -9,10 +9,10 @@ typedef enum e_bool
 	TRUE = 1
 }	t_bool;
 
-# define EVEN(nbr)	((nbr) % 2 == 0)
+t_bool	is_even(int nbr);
 
-# define EVEN_MSG	"I have an even number of arguments.\n"
-# define ODD_MSG	"I have an odd number of arguments.\n"
-# define SUCCESS	0
+# define EVEN_MSG		"I have an even number of arguments.\n"
+# define ODD_MSG		"I have an odd number of arguments.\n"
+# define SUCCESS		0
 
 #endif
