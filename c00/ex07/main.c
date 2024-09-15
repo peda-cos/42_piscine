b059c285954f6d71535f8392b91cbb880269fde4
Spuko <83892026+SpukoBR@users.noreply.github.com>
2024-09-15 11:31:25 -0300

    feat: Makefile and main.c

---
+++ c00/ex07/main.c
@@ -0,0 +1,15 @@
+void    ft_putchar(char c);
+void	ft_putnbr(int nb);
+
+int	main(void)
+{
+	ft_putnbr(42);
+	ft_putchar('\n');
+	ft_putnbr(-42);
+	ft_putchar('\n');
+	ft_putnbr(0);
+	ft_putchar('\n');
+	ft_putnbr(-2147483648);
+	ft_putchar('\n');
+	return (0);
+}
